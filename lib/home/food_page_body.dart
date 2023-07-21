c07c8a3eba819f8724253ecdbf66d6b478ed5a7b
eduardoagas <eagasilva12@gmail.com>
2023-07-21 07:55:28 -0300

    thir commit rs

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -14,8 +14,8 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
   var _currPageValue = 0.0;
-  double _scaleFactor = 0.8;
-  double _height = 220;
+  final double _scaleFactor = 0.8;
+  final double _height = 220;
 
   @override
   void initState() {
@@ -36,7 +36,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 320,
       child: PageView.builder(
           controller: pageController,
@@ -48,7 +48,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    Matrix4 matrix = new Matrix4.identity();
+    Matrix4 matrix = Matrix4.identity();
     if (index == _currPageValue.floor()) {
       var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
       var currTrans = _height * (1 - currScale) / 2;
@@ -107,7 +107,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                         children: [
                           Wrap(
                             children: List.generate(5, (index) {
-                              return Icon(
+                              return const Icon(
                                 Icons.star,
                                 color: AppColors.mainColor,
                                 size: 15,
@@ -131,7 +131,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                       const SizedBox(
                         height: 20,
                       ),
-                      Row(
+                      const Row(
                         children: [
                           IconAndTextWidget(
                               icon: Icons.circle_sharp,
--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -20,41 +20,39 @@ class _MainFoodPageState extends State<MainFoodPage> {
           Container(
             margin: const EdgeInsets.only(top: 45, bottom: 15),
             padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      BigText(
-                        text: "Bangladesh",
-                        color: AppColors.mainColor,
-                        size: 30,
-                      ),
-                      Row(
-                        children: [
-                          SmallText(
-                            text: "Narshingdi",
-                            color: Colors.black54,
-                          ),
-                          const Icon(Icons.arrow_drop_down_rounded)
-                        ],
-                      )
-                    ],
-                  ),
-                  Center(
-                    child: Container(
-                      width: 45,
-                      height: 45,
-                      child: const Icon(Icons.search, color: Colors.white),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: AppColors.mainColor,
-                      ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    BigText(
+                      text: "Bangladesh",
+                      color: AppColors.mainColor,
+                      size: 30,
                     ),
-                  )
-                ],
-              ),
+                    Row(
+                      children: [
+                        SmallText(
+                          text: "Narshingdi",
+                          color: Colors.black54,
+                        ),
+                        const Icon(Icons.arrow_drop_down_rounded)
+                      ],
+                    )
+                  ],
+                ),
+                Center(
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
+                    ),
+                    child: const Icon(Icons.search, color: Colors.white),
+                  ),
+                )
+              ],
             ),
           ),
           const FoodPageBody(),
